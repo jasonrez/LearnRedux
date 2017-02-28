8fea228edeb4455cb92aed14996c0643fb177a83
jasonrez <jasonrez@hotmail.co.uk>
2017-02-28 21:05:16 +0000

    Break up reducer into multiple reducers

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,63 +1,64 @@
 
 // let redux = require('redux')
-import { createStore, compose } from 'redux'
+import { createStore, compose, combineReducers} from 'redux'
 
 
 console.log('starting redux example')
 
-let stateDefault = {
-  name: 'Anonymous',
-  hobbies: [],
-  movies: []
-}
-
 let nextHobbyId = 1
 let nextMovieId = 1
 
-let reducer = (state = stateDefault, action) => {
-  switch (action.type) {
-    case 'CHANGE_NAME':
-      return {
-        ...state,
-        name: action.name
-      }
-    case 'ADD_HOBBY' :
-      return {
-        ...state,
-        hobbies: [
-          ...state.hobbies,
-          {
-            id: nextHobbyId++,
-            bobby: action.hobby
-          }
-        ]
-      }
-    case 'REMOVE_HOBBY' :
-      return {
-        ...state,
-        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
-      }
-    case 'ADD_MOVIE' :
-      return {
-        ...state,
-        movies:[
-          ...state.movies,
-          {
-            id : nextMovieId++,
-            title : action.title,
-            genre : action.genre
-          }
-        ]
-      }
-    case 'REMOVE_MOVIE' :
-      return {
-        ...state,
-        movies: state.movies.filter(movie => movie.id !== action.id)
-      }
-      default:
-        return state
+
+
+let nameReducer = (state = 'Anon', action) => {
+  switch(action.type) {
+    case 'CHANGE_NAME' :
+      return action.name
+    default:
+      return state
   }
 }
+
+let hobbiesReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_HOBBY' :
+      return [
+        ...state,
+        {
+          id : nextHobbyId++,
+          hobby : action.hobby
+        }
+      ]
+      case 'REMOVE_HOBBY' :
+        return state.filter( hobby => hobby.id !== action.id)
+    default:
+      return state
+    }
+}
+
+let moviesReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_MOVIE' :
+      return [
+        ...state,
+        {
+          id : nextMovieId++,
+          title : action.title,
+          genre : action.genre
+        }
+      ]
+    case 'REMOVE_MOVIE' :
+      return state.filter( movie =>  movie.id !== action.id )
+    default:
+      return state
+  }
+}
+
+let reducer = combineReducers({
+  name : nameReducer,
+  hobbies : hobbiesReducer,
+  movies : moviesReducer
+})
 
 let store = createStore(reducer, compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
@@ -75,19 +76,20 @@ let unsubscribe = store.subscribe(() => {
 // unsubscribe();
 
 
+
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+})
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Swimming'
+})
 store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Jason Rez'
-})
-
-store.dispatch({
-  type: 'ADD_HOBBY',
-  bobby: 'Running'
-})
-
-store.dispatch({
-  type: 'ADD_HOBBY',
-  bobby: 'Swimming'
 })
 
 store.dispatch({
